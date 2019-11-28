75c6e88de11c9410e61ad5170dc5a5b0ba93f411
Brendan <bjregnery@ucdavis.edu>
2019-11-28 11:47:54 -0600

    added 2017 QCD MC

--- preprocess/test/run_QCD.py
+++ preprocess/test/run_QCD.py
@@ -1,4 +1,13 @@
-import glob
+#=========================================================================================
+# run_QCD.py -----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
+# Authors: Brendan Regnery, Reyer Band ---------------------------------------------------
+#-----------------------------------------------------------------------------------------
+
+#=========================================================================================
+# Load Modules and Settings --------------------------------------------------------------
+#=========================================================================================
+
 import FWCore.ParameterSet.Config as cms
 from JMEAnalysis.JetToolbox.jetToolbox_cff import jetToolbox
 from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
@@ -9,32 +18,30 @@ process = cms.Process("run")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("JetMETCorrections.Configuration.JetCorrectionServices_cff")
 process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load("RecoBTag.Configuration.RecoBTag_cff")
 
 process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_v4')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1),
                                         allowUnscheduled = cms.untracked.bool(True))
 
-# Get file names with unix pattern expander
-#files = glob.glob("/afs/cern.ch/work/b/bregnery/public/HHwwwwMCgenerator/CMSSW_8_0_21/src/hhMCgenerator/RootFiles/M3500/*.root")
-# Add to the beginning of each filename
-#for ifile in range(len(files)):
-#    files[ifile] = "file:" + files[ifile]
-
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-	#'root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/70000/847868B8-7E26-E611-9BEA-02163E01413E.root'
-	#'/store/mc/RunIISpring16MiniAODv2/QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/70000/847868B8-7E26-E611-9BEA-02163E01413E.root'
-#	'/store/mc/RunIISpring16MiniAODv2/QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/70000/065ACA52-3D1C-E611-97A4-44A84225CFF0.root'
-	#'/store/mc/RunIISummer16MiniAODv2/QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/110000/E0C50E83-BFB1-E611-9228-0025905B85D2.root '
+        # This single file can be used for testing
+        #'root://cmsxrootd-site.fnal.gov//store/mc/RunIIFall17MiniAODv2/QCD_Pt_300to470_TuneCP5_13TeV_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v3/00000/00B3942F-2DBC-E811-8AF2-0CC47AFC3C9C.root'
 	)
 )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+#=========================================================================================
+# Remake the Jet Collections -------------------------------------------------------------
+#=========================================================================================
 
 # Adjust the jet collection to include tau4
 jetToolbox( process, 'ak8', 'jetsequence', 'out',
@@ -44,7 +51,11 @@ jetToolbox( process, 'ak8', 'jetsequence', 'out',
     maxTau = 4
 )
 
-# Apply a jet preselection
+#=========================================================================================
+# Prepare and run producer ---------------------------------------------------------------
+#=========================================================================================
+
+# Apply a preselction
 process.selectedAK8Jets = cms.EDFilter('PATJetSelector',
     src = cms.InputTag('selectedPatJetsAK8PFCHS'),
     cut = cms.string('pt > 100.0 && abs(eta) < 2.4'),
@@ -61,7 +72,8 @@ process.countAK8Jets = cms.EDFilter("PATCandViewCountFilter",
 # Run the producer
 process.run = cms.EDProducer('BESTProducer',
 	inputJetColl = cms.string('selectedAK8Jets'),
-        isSignal = cms.bool(False)
+        jetType = cms.string('Q'),
+        jetColl = cms.string('CHS')
 )
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string("preprocess_BEST_QCD.root") )
@@ -70,6 +82,7 @@ process.out = cms.OutputModule("PoolOutputModule",
                                fileName = cms.untracked.string("ana_out.root"),
                                #SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
                                outputCommands = cms.untracked.vstring('drop *',
+                                                                      #'keep *_*AK8*_*_*', #'drop *',
                                                                       'keep *_*run*_*_*'
                                                                       #, 'keep *_goodPatJetsCATopTagPF_*_*'
                                                                       #, 'keep recoPFJets_*_*_*'
@@ -78,4 +91,8 @@ process.out = cms.OutputModule("PoolOutputModule",
 process.outpath = cms.EndPath(process.out)
 
 # Organize the running process
-process.p = cms.Path(process.selectedAK8Jets*process.countAK8Jets*process.run)
+process.p = cms.Path(
+    process.selectedAK8Jets
+    * process.countAK8Jets
+    * process.run
+)
