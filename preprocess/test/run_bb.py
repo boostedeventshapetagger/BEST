be03fdfc59d8ac854915765f22165e491561477d
Reyer <rband@ucdavis.edu>
2020-03-05 16:51:45 -0600

    Added some pre-selections to the producer directly and not as a module in the run scripts, removed those modules

--- preprocess/test/run_bb.py
+++ preprocess/test/run_bb.py
@@ -3,6 +3,8 @@ from JMEAnalysis.JetToolbox.jetToolbox_cff import jetToolbox
 from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
 from Configuration.AlCa.GlobalTag import GlobalTag
 
+
+GT = '94X_mc2017_realistic_v17'
 process = cms.Process("run")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -11,7 +13,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 process.load("JetMETCorrections.Configuration.JetCorrectionServices_cff")
 process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
-process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v17')
+process.GlobalTag = GlobalTag(process.GlobalTag, GT)
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
 
@@ -23,32 +25,33 @@ process.source = cms.Source("PoolSource",
         )
                             )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-jetToolbox( process, 'ak8', 'jetsequence', 'out',
-    updateCollection = 'slimmedJetsAK8',
-    JETCorrPayload= 'AK8PFPuppi',
-    PUMethod='Puppi',
-    runOnMC=True,    
-#    JETCorrPayload= 'AK8PFchs',
-    addNsub = True,
-    maxTau = 4
-)
+
+# jetToolbox( process, 'ak8', 'jetsequence', 'out',
+#     updateCollection = 'slimmedJetsAK8',
+#     JETCorrPayload= 'AK8PFPuppi',
+#     PUMethod='Puppi',
+#     runOnMC=True,    
+# #    JETCorrPayload= 'AK8PFchs',
+#     addNsub = True,
+#     maxTau = 4
+# )
 
 
 process.selectedAK8Jets = cms.EDFilter('PATJetSelector',
-    src = cms.InputTag('selectedPatJetsAK8PFPuppi'),
-    cut = cms.string('pt > 300.0 && abs(eta) < 2.4'),
-    filter = cms.bool(True)
-)
+                                       src = cms.InputTag('slimmedJetsAK8'),
+                                       cut = cms.string('pt > 500.0 && abs(eta) < 2.4'),
+                                       filter = cms.bool(True)
+                                       )
 
 process.countAK8Jets = cms.EDFilter("PATCandViewCountFilter",
-    minNumber = cms.uint32(1),
-    maxNumber = cms.uint32(99999),
-    src = cms.InputTag("selectedAK8Jets"),
-    filter = cms.bool(True)
-)
+                                    minNumber = cms.uint32(1),
+                                    maxNumber = cms.uint32(99999),
+                                    src = cms.InputTag('slimmedJetsAK8')
+#                                    filter = cms.bool(True)
+                                    )
 
 process.run = cms.EDProducer('BESTProducer',
-	inputJetColl = cms.string('selectedAK8Jets'),
+	inputJetColl = cms.string('slimmedJetsAK8'),
         jetColl = cms.string('PUPPI'),                     
         jetType = cms.string('b')
 #	pdgIDforMatch = cms.int32(23),
@@ -75,3 +78,4 @@ process.out = cms.OutputModule("PoolOutputModule",
 process.outpath = cms.EndPath(process.out)
 
 process.p = cms.Path(process.selectedAK8Jets*process.countAK8Jets*process.run)
+
