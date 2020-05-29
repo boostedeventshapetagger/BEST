4947f09ca633ada93ea45557a099059bf341283e
AFJohan92 <johan.sebastian.bonilla@cern.ch>
2020-05-29 13:38:15 -0500

    Working sci-kit-learn splitter. MUCH faster.

--- formatConverter/sampleSplitter.py
+++ formatConverter/sampleSplitter.py
@@ -5,7 +5,7 @@
 # This program splits h5f files into 3 smaller but equal orthogonal files       ///
 # The point is to create separate train/validation/test samples of equal size   ///
 # Inputs should be h5f files (can be flatted, or staright from formatConverter) ///
-# Output should be three sets of hd5f files: training, validation, testign      ///
+# Output should be three sets of hd5f files: training, validation, testing      ///
 #----------------------------------------------------------------------------------
 
 # modules
@@ -21,6 +21,7 @@ import argparse
 import os
 import random
 import time
+from sklearn.model_selection import train_test_split
 
 # User modules
 import tools.imageOperations as img
@@ -29,10 +30,56 @@ import tools.imageOperations as img
 root.gROOT.SetBatch(True)
 
 # Global variables
-listOfSampleTypes = ["b","Higgs","QCD","Top","W","Z"]
+listOfSamples = ["b","Higgs","QCD","Top","W","Z"]
 
 # Helper functions
-def splitFile(inputPath, debug):
+def splitFileSKL(inputPath, debug):
+    print("Starting clock")
+    startTime = time.time()
+    
+    listOfFrameTypes = ["Higgs","Top","W","Z"]
+    setTypes = ["train", "validation", "test"]
+
+    # Open file, grab keys
+    inputFile = h5py.File(inputPath,"r")
+    dataKeys = inputFile.keys()
+
+    # Create data frame and output files to handle copied information
+    # Make h5f file to store the images and BES variables
+    h5fTrain = h5py.File(inputPath.split('.')[0]+"_train.h5","w")
+    h5fValidation = h5py.File(inputPath.split('.')[0]+"_validation.h5","w")
+    h5fTest = h5py.File(inputPath.split('.')[0]+"_test.h5","w")
+        
+    besData = {}
+    outputFiles = {}
+    for setType in setTypes:
+        besData[setType] = {}
+        #outputFiles[setType] = h5py.File(inputPath.split('.')[0]+"_"+setType+".h5","w")
+
+    print("Starting key loop")
+    for myKey in dataKeys:
+        keyTime = time.time()
+        dsetH5 = inputFile[myKey]
+        dsetNP = np.array(dsetH5)
+        print("NPdset creation time:", time.time()-keyTime)
+        output1 = train_test_split(dsetNP, train_size=0.33, shuffle=True)
+        output2 = train_test_split(output1[0], train_size=0.5, shuffle=True)
+        print("Outdset creation time:", time.time()-keyTime)
+        if "frame" in myKey.lower():
+            besData["train"][myKey] = h5fTrain.create_dataset(myKey, data=output1[0], maxshape=(None, inputFile[myKey].shape[1], inputFile[myKey].shape[2], inputFile[myKey].shape[3]), compression='lzf')
+            besData["validation"][myKey] = h5fValidation.create_dataset(myKey, data=output2[0], maxshape=(None, inputFile[myKey].shape[1], inputFile[myKey].shape[2], inputFile[myKey].shape[3]), compression='lzf')
+            besData["test"][myKey] = h5fTest.create_dataset(myKey, data=output2[1], maxshape=(None, inputFile[myKey].shape[1], inputFile[myKey].shape[2], inputFile[myKey].shape[3]), compression='lzf')
+            print("DS store time:", time.time()-keyTime)
+        else:
+            besData["train"][myKey] = h5fTrain.create_dataset(myKey, data=output1[0], maxshape=(None, inputFile[myKey].shape[1]), compression='lzf')
+            besData["validation"][myKey] = h5fValidation.create_dataset(myKey, data=output2[0], maxshape=(None, inputFile[myKey].shape[1]), compression='lzf')
+            besData["test"][myKey] = h5fTest.create_dataset(myKey, data=output2[1], maxshape=(None, inputFile[myKey].shape[1]), compression='lzf')
+            print("DS store time:", time.time()-keyTime)
+        print("Key iteration time:", time.time()-keyTime)
+        keyTime = time.time()
+    print("Splitting time:", time.time()-startTime)
+
+def splitFileSlow(inputPath, debug):
     listOfFrameTypes = ["Higgs","Top","W","Z"]
     setTypes = ["train", "validation", "test"]
 
@@ -75,7 +122,7 @@ def splitFile(inputPath, debug):
             setMask = (split==i)
             if debug:
                 print(setTypes[i]+" mask shape:", setMask.shape)
-                print("Time to make mask:", time.time()-keyTime)
+                print("Time to make mask:", time.time()-setTime)
 
             setTime = time.time()
             print("Begin "+setTypes[i]+" split")
@@ -86,7 +133,7 @@ def splitFile(inputPath, debug):
             # The ellipses are used for shape compability when masking. It interprets the dimensions needed.
             # setData is the masked data. So it should be ~1/3 of the size of keyData.
             setData = keyData[setMask,...]
-            if debug: print("Time to copy data:", time.time()-keyTime)
+            if debug: print("Time to copy data:", time.time()-setTime)
 
             setTime = time.time()
             print("Begin saving data")
@@ -125,7 +172,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if not args.samples == "all": listOfSamples = args.samples.split(',')
     if args.debug:
-        print("Samples to process: ", listOfSampleTypes)
+        print("Samples to process: ", listOfSamples)
 
     # Make directories you need
     if not os.path.isdir(args.h5Dir): print(args.h5Dir, "does not exist")
@@ -133,7 +180,8 @@ if __name__ == "__main__":
     for sampleType in listOfSamples:
         print("Processing", sampleType)
         inputPath = args.h5Dir+sampleType+"Sample_BESTinputs.h5"
-        splitFile(inputPath, args.debug)
+        #splitFileSlow(inputPath, args.debug)
+        splitFileSKL(inputPath, args.debug)
         
         
     ## Plot total pT distributions
